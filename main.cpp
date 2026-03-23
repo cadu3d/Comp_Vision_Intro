ec772281929858991fc27d7f9ef9ee2bbd114068
cadu3d <cadu3d@gmail.com>
2026-03-22 22:35:16 -0300

    Commit Ex 2

--- main.cpp
+++ main.cpp
@@ -1,55 +1,62 @@
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void ChooseExercise(int exercise);
-void Exercise1(int exercise);
-void Exercise2(int exercise);
-void Exercise3(int exercise);
-void Exercise4(int exercise);
-void Exercise5(int exercise);
-void Exercise6(int exercise);
+void userInput();
+void runExercise(int exercise);
+void Exercise1();
+void Exercise2();
+void Exercise3();
+void Exercise4();
+void Exercise5();
+void Exercise6();
 
 int main()
 {
-    int exercise;
-    auto lang = "C++";
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_ERROR);
 
-    std::cout << "Hello and welcome to Visual Computing Lab" << lang << "!\n";
+    std::cout << "Introducao a Computacao Grafica Lab 1 " "!\n";
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
-    std::cout << "Selecione um exercicio de 1 a 8" << std::endl;
-    std::cin >> exercise;
-
-    ChooseExercise(exercise);
+    userInput();
 }
 
-void ChooseExercise(int exercise)
+void userInput()
+{
+    int exercise;
+
+    std::cout << "\n";
+    std::cout << "Selecione um exercicio de 1 a 8" << std::endl;
+    std::cin >> exercise;
+    runExercise(exercise);
+}
+
+void runExercise(int exercise)
 {
     switch (exercise)
     {
     case 1:
-        std::cout << "Rodando o Exercício 1" << std::endl;
+        Exercise1();
         break;
     case 2:
-        std::cout << "Rodando o Exercicio 2" << std::endl;
+        Exercise2();
         break;
     case 3:
-        std::cout << "Rodando o Exercicio 3" << std::endl;
+        Exercise3();
         break;
     case 4:
-        std::cout << "Rodando o Exercicio 4" << std::endl;
+        Exercise3();
         break;
     case 5:
-        std::cout << "Rodando o Exercicio 5" << std::endl;
+        Exercise3();
         break;
     case 6:
-        std::cout << "Rodando o Exercicio 6" << std::endl;
+        Exercise3();
         break;
     case 7:
-        std::cout << "Rodando o Exercicio 7" << std::endl;
+        Exercise3();
         break;
     case 8:
-        std::cout << "Rodando o Exercicio 8" << std::endl;
+        Exercise3();
         break;
     default:
         std::cout << "Invalid exercise" << std::endl;
@@ -57,44 +64,89 @@ void ChooseExercise(int exercise)
     }
 }
 
-
-void Exercise1(int exercise)
+void mostrarImagem(std::string janela, cv::Mat imageRGB)
 {
-    std::cout << "Run Exercise " << exercise << std::endl;
+    cv::imshow(janela, imageRGB);
+    cv::waitKey(2500);
+    cv::destroyWindow(janela);
 }
 
-void Exercise2(int exercise1)
+std::string buscarImagem()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::string pathImage;
+    std::string imagem = "imagem.jpg";
+    pathImage = "../sourceAssets/" + imagem;
+    return pathImage;
+}
+
+void Exercise1()
+{
+    std::cout << "Rodando o Exercicio 1" << std::endl;
+
+    std::string pathImage = buscarImagem();
+
+    //Mostrar imagem
+    cv::Mat image = cv::imread(pathImage, cv::IMREAD_COLOR);
+    mostrarImagem("Mostrar imagem", image);
+
+    //Mostrar Largura, Altura e Numero de Canais
+    std::cout << "A imagem tem resolucao de: " << image.rows << " x " << image.cols
+        << " e " << image.channels() << " canais. " << std::endl;
+
+    userInput();
 }
 
 
-void Exercise3(int exercise1)
+void Exercise2()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::cout << "Rodando o Exercicio 2" << std::endl;
+
+    std::string pathImage = buscarImagem();
+
+    //Armazenar imagem na varialvel
+    cv::Mat imageRGB = cv::imread(pathImage, cv::IMREAD_COLOR);
+
+    //Converter para grayscale e gravar em disco
+    //duvida: usar IMREAD_GRAYSCALE OU RGB2GRAY?
+    cv::Mat imageGray;
+    cv::cvtColor(imageRGB, imageGray, cv::COLOR_RGB2GRAY);
+    cv::imwrite("../output/imagem_gray.jpg", imageGray);
+    std::cout << "A imagem em tons de cinza foi gravada em disco" << std::endl;
+
+    //Mostrar imagem colorida e convertida para tons de cinza
+    mostrarImagem("Colorida", imageRGB);
+    mostrarImagem("Tons de Cinza", imageGray);
+
+    userInput();
 }
 
-void Exercise4(int exercise1)
+
+void Exercise3()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::cout << "Rodando o Exercicio 3" << std::endl;
 }
 
-void Exercise5(int exercise1)
+void Exercise4()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::cout << "Rodando o Exercicio 4" << std::endl;
 }
 
-void Exercise6(int exercise1)
+void Exercise5()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::cout << "Rodando o Exercicio 5" << std::endl;
 }
 
-void Exercise7(int exercise1)
+void Exercise6()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::cout << "Rodando o Exercicio 6" << std::endl;
 }
 
-void Exercise8(int exercise1)
+void Exercise7()
 {
-    std::cout << "Run Exercise " << exercise1 << std::endl;
+    std::cout << "Rodando o Exercicio 7" << std::endl;
+}
+
+void Exercise8()
+{
+    std::cout << "Rodando o Exercicio 8" << std::endl;
 }
